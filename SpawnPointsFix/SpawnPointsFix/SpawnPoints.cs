c85921fb46e3a31fccde2ccbf733638d3d8801df
Egor Aralov <aei.omsk@gmail.com>
2017-05-27 21:04:03 +0200

    Update for 1.7.0

--- SpawnPointsFix/SpawnPointsFix/SpawnPoints.cs
+++ SpawnPointsFix/SpawnPointsFix/SpawnPoints.cs
@@ -16,10 +16,31 @@ namespace SpawnPointsFix
             {
                 return;
             }
-            var spawnPoints = (from path in paths
+            var allSpawnPoints = (from path in paths
                 where IsVehicleStop(path)
-                select GetMiddle(path)).Distinct().ToArray();
-            switch (spawnPoints.Length)
+                select new KeyValuePair<Vector3, ItemClass>(GetMiddle(path), path.m_netInfo.m_class)).Distinct().ToArray();
+            Vector3[] primarySpawnPoints;
+            Vector3[] secondarySpawnPoints;
+
+            if (buildingAI.m_secondaryTransportInfo == null)
+            {
+                primarySpawnPoints = allSpawnPoints.Select(kvp => kvp.Key).ToArray();
+                secondarySpawnPoints = new Vector3[] { };
+            }
+            else
+            {
+                primarySpawnPoints = allSpawnPoints.
+                    Where(kvp => kvp.Value.m_subService == buildingAI.m_transportInfo.m_class.m_subService || kvp.Value.m_subService == ItemClass.SubService.None && kvp.Value.m_service == ItemClass.Service.Road && buildingAI.m_transportInfo.m_class.m_subService == ItemClass.SubService.PublicTransportBus).
+                    Select(kvp => kvp.Key).
+                    ToArray(); 
+                secondarySpawnPoints = allSpawnPoints.
+                    Where(kvp => kvp.Value.m_subService == buildingAI.m_secondaryTransportInfo.m_class.m_subService || kvp.Value.m_subService == ItemClass.SubService.None && kvp.Value.m_service == ItemClass.Service.Road && buildingAI.m_secondaryTransportInfo.m_class.m_subService == ItemClass.SubService.PublicTransportBus).
+                    Select(kvp => kvp.Key).
+                    ToArray();
+            }
+
+
+            switch (primarySpawnPoints.Length)
             {
                 case 0:
                     buildingAI.m_spawnPosition = Vector3.zero;
@@ -27,21 +48,51 @@ namespace SpawnPointsFix
                     buildingAI.m_spawnPoints = new DepotAI.SpawnPoint[] { };
                     break;
                 case 1:
-                    buildingAI.m_spawnPosition = spawnPoints[0];
-                    buildingAI.m_spawnTarget = spawnPoints[0];
+                    buildingAI.m_spawnPosition = primarySpawnPoints[0];
+                    buildingAI.m_spawnTarget = primarySpawnPoints[0];
                     buildingAI.m_spawnPoints = new[]
                     {
                         new DepotAI.SpawnPoint
                         {
-                            m_position =  spawnPoints[0],
-                            m_target =  spawnPoints[0]
+                            m_position =  primarySpawnPoints[0],
+                            m_target =  primarySpawnPoints[0]
                         }
                     };
                     break;
                 default:
                     buildingAI.m_spawnPosition = Vector3.zero;
                     buildingAI.m_spawnTarget = Vector3.zero;
-                    buildingAI.m_spawnPoints = spawnPoints.Select(p => new DepotAI.SpawnPoint
+                    buildingAI.m_spawnPoints = primarySpawnPoints.Select(p => new DepotAI.SpawnPoint
+                    {
+                        m_position = p,
+                        m_target = p
+                    }).ToArray();
+                    break;
+            }
+
+            switch (secondarySpawnPoints.Length)
+            {
+                case 0:
+                    buildingAI.m_spawnPosition2 = Vector3.zero;
+                    buildingAI.m_spawnTarget2 = Vector3.zero;
+                    buildingAI.m_spawnPoints2 = new DepotAI.SpawnPoint[] { };
+                    break;
+                case 1:
+                    buildingAI.m_spawnPosition2 = secondarySpawnPoints[0];
+                    buildingAI.m_spawnTarget2 = secondarySpawnPoints[0];
+                    buildingAI.m_spawnPoints2 = new[]
+                    {
+                        new DepotAI.SpawnPoint
+                        {
+                            m_position = secondarySpawnPoints[0],
+                            m_target = secondarySpawnPoints[0]
+                        }
+                    };
+                    break;
+                default:
+                    buildingAI.m_spawnPosition2 = Vector3.zero;
+                    buildingAI.m_spawnTarget2 = Vector3.zero;
+                    buildingAI.m_spawnPoints2 = secondarySpawnPoints.Select(p => new DepotAI.SpawnPoint
                     {
                         m_position = p,
                         m_target = p
@@ -56,7 +107,7 @@ namespace SpawnPointsFix
         }
 
 
-        private static Vector3 GetMiddle(BuildingInfo.PathInfo path)
+        private static Vector3 GetMiddle(BuildingInfo.PathInfo path) //TODO(earalov): properly handle curved paths
         {
             return (path.m_nodes.First() + path.m_nodes.Last()) / 2;
         }
